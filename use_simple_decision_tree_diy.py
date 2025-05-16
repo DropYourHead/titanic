4c17ffdb354ea6ebac6b344817c711384bb0904a
刘宇翔 <liuyuxiang@liuyuxiangs-MacBook-Pro.local>
2025-05-16 23:57:07 +0800

    fix some of the logic

--- use_simple_decision_tree_diy.py
+++ use_simple_decision_tree_diy.py
@@ -1,17 +1,18 @@
 import pandas as pd
-import numpy
+import numpy as np
 
 class DNode:
-    def __init__(self, feature=None, val=None):
+    def __init__(self, feature=None, X=None, y=None):
         #feature : the feature we choose to split, a string, leaf node if feature is None
         #value : the value in the node, dataframe
-        self.value = val
+        self.X = X
+        self.y = y
         self.feature = feature
         self.pList = {}
-        self.valueDiv(feature, val)
+        self.valueDiv(feature, X)
         
-    def valueDiv(self, feature, val):
-        div = val.groupby(feature)
+    def valueDiv(self, feature, X):
+        div = X.groupby(feature)
         for k, v in div:
             self.pList[k] = DNode(feature=None, val=v)
         
@@ -19,12 +20,17 @@ class DNode:
 
 
 class DTree:
-    def __init__(self, maxDepth, threshold, minNum, val):
+    def __init__(self, maxDepth, threshold, minNum):
         self.root = None
         self.maxDepth = maxDepth
         self.threshold = threshold
         self.minNum = minNum
-        self.val = val
 
     def calcEntropy(self, features, val):
+        pass
 
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
